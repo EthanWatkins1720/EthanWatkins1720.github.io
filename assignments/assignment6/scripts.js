83c860d7ab7c9ead18544f0e6feec1fdc7551bbb
Zandair Watkins <ethanwatkins1720@gmail.com>
2023-09-17 22:04:55 -0400

    Assignment 6 animate

--- assignments/assignment6/scripts.js
+++ assignments/assignment6/scripts.js
@@ -1,9 +1,22 @@
 const moveSnail = () => {
-    document.getElementById("snail").classList.add("move-sail");
+    document.getElementById("snail").classList.add("move-snail");
+}
+
+const hideImage = () => {
+    document.getElementById("togglable-img").style.display = "none";
+}
+
+const showImage = () => {
+    document.getElementById("togglable-img").style.display = "block";
 }
 
 
+const sayHi = () => {
+    alert("Hi!");
+}
 
 window.onload = () => {
+    document.getElementById("hide-button").onclick = hideImage;
+    document.getElementById("show-button").onclick = showImage;
     document.getElementById("move-button").onclick = moveSnail;
 }
