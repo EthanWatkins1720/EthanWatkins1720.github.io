763e3fb66965c9eae42601ea2b3a959c5b941bc0
Zandair Watkins <ethanwatkins1720@gmail.com>
2023-09-17 23:01:12 -0400

    Assignment 6 comment

--- assignments/assignment6/scripts.js
+++ assignments/assignment6/scripts.js
@@ -10,6 +10,25 @@ const showImage = () => {
     document.getElementById("togglable-img").style.display = "block";
 }
 
+const displayComment = () => {
+    const fin = document.createElement("div");
+    const proName = document.getElementById("product-name").value;
+    const comment = document.getElementById("comment").value;
+    const rating = document.getElementById("rating").value;
+    const username = document.getElementById("username").value;
+    fin.classList.add("output");
+    fin.appendChild(proName);
+    fin.appendChild(document.createElement("br"));
+    fin.appendChild(rating);
+    fin.appendChild("/5 	&#11088; " + comment);
+    fin.appendChild(document.createElement("br"));
+    fin.appendChild("By: " + username);
+    document.getElementById("final-comment").appendChild(fin);
+}
+
+const generateTable = () => {
+
+}
 
 const sayHi = () => {
     alert("Hi!");
@@ -19,4 +38,5 @@ window.onload = () => {
     document.getElementById("hide-button").onclick = hideImage;
     document.getElementById("show-button").onclick = showImage;
     document.getElementById("move-button").onclick = moveSnail;
+    document.getElementById("comment-button").onclick = displayComment;
 }
