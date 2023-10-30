a8d738dae51f1681c876dc4a1c605916c0cf54d6
Zandair Watkins <ethanwatkins1720@gmail.com>
2023-10-29 20:11:10 -0400

    Assignment 11 partial

--- assignments/assignment11/scripts.js
+++ assignments/assignment11/scripts.js
@@ -13,14 +13,24 @@ const showMovies = async() => {
     let movies = await getMovies();
     let moviesSection = document.getElementById("movies-section");
 
-    movies.forEach(
-        movie => moviesSection.append(getMovieItem(movie))
+    movies.forEach((movie) => 
+        moviesSection.append(getMovieItem(movie))
     );
 }
 
 const getMovieItem = (movie) => {
     let movieSection = document.createElement("section");
     movieSection.classList.add("movie");
+
+    let row = document.createElement("div");
+
+    let img = document.createElement("img");
+    img.src = movie.img;
+    movieSection.append(img);
+
+    let h3 = document.createElement("h3");
+    h3.innerText = movie.name;
+    movieSection.append(h3);
 
     return movieSection;
 }
